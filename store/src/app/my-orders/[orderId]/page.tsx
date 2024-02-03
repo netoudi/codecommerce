ae8b4823ea91273f3c8112ddc0ebdbcc6e3107be
netoudi <netoudi@rocketmail.com>
2024-02-03 10:46:00 -0300

    refactor(store): update my orders

--- store/src/app/my-orders/[orderId]/page.tsx
+++ store/src/app/my-orders/[orderId]/page.tsx
@@ -1,44 +1,12 @@
 import { Box, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 import { Total } from '@/components/total';
-import { Order, OrderStatus } from '@/models';
-
-const order: Order = {
-  id: '1',
-  status: OrderStatus.PENDING,
-  created_at: '2021-10-10T00:00:00.000Z',
-  items: [
-    {
-      id: 1,
-      product: {
-        id: '1',
-        name: 'Camisa',
-        description: 'Camisa branca',
-        price: 100,
-        image_url: 'https://source.unsplash.com/random?product',
-        category_id: '1',
-      },
-      quantity: 2,
-      price: 100,
-    },
-    {
-      id: 2,
-      product: {
-        id: '2',
-        name: 'Calça',
-        description: 'Calça jeans',
-        price: 100,
-        image_url: 'https://source.unsplash.com/random?product',
-        category_id: '1',
-      },
-      quantity: 1,
-      price: 100,
-    },
-  ],
-  total: 1000,
-};
+import { OrderStatus } from '@/models';
+import { OrderServiceFactory } from '@/services/order.service';
 
 export default async function MyOrderDetail({ params }: { params: { orderId: string } }) {
+  const order = await OrderServiceFactory.create().getOrder(params.orderId);
+
   return (
     <Box>
       <Grid2 container spacing={2}>
