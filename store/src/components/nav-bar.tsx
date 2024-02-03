8f11b21bd73c0e1743e686d089a06aa5f27e4f5e
netoudi <netoudi@rocketmail.com>
2024-02-03 17:17:17 -0300

    feat(store): show total items of cart in navbar

--- store/src/components/nav-bar.tsx
+++ store/src/components/nav-bar.tsx
@@ -2,16 +2,18 @@ import Image from 'next/legacy/image';
 import Link from 'next/link';
 import HomeIcon from '@mui/icons-material/Home';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
+import { AppBar, Badge, Box, IconButton, Toolbar, Typography } from '@mui/material';
 import { SearchBar } from '@/components/search-bar';
 import { SelectCategory } from '@/components/select-category';
 import { UserMenu } from '@/components/user-menu';
 import { AuthService } from '@/services/auth.service';
+import { CartServiceFactory } from '@/services/cart.service';
 import { CategoryService } from '@/services/category.service';
 
 export async function Navbar() {
   const categories = await new CategoryService().getCategories();
   const user = new AuthService().getUser();
+  const cart = CartServiceFactory.create().getCart();
 
   return (
     <AppBar position="fixed">
@@ -23,7 +25,9 @@ export async function Navbar() {
           <SearchBar />
         </Box>
         <IconButton LinkComponent={Link} size="large" href="/my-cart">
-          <ShoppingCartIcon />
+          <Badge badgeContent={cart.items.length} color="primary">
+            <ShoppingCartIcon />
+          </Badge>
         </IconButton>
         <UserMenu user={user} />
       </Toolbar>
